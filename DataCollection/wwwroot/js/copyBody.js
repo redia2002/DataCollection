667e0d679b8dc06c7225a1027dffbfd685b72ec9
Haruyuki Takemura <hatakemu@microsoft.com>
2019-12-18 22:45:27 +0900

    12/18 変更

---
+++ DataCollection/wwwroot/js/copyBody.js
@@ -0,0 +1,10 @@
+﻿function cp() {
+    var element = document.body;
+    var rng = document.createRange();
+    rng.selectNodeContents(element);
+    window.getSelection().addRange(rng);
+    document.execCommand('copy');
+    element = null;
+    rng = null;
+    alert('Copied to the clipboard.');
+}
